406cd750c0d1117f344fab22b7cab07627082ac9
JGSides <67124891+JGSides@users.noreply.github.com>
2021-07-14 10:20:04 -0700

    Transitioned to HDRP and added new player movement system

---
+++ project-middleisle/Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerMove : MonoBehaviour, IsoMove.IPlayerActions
+{
+    public CharacterController controller;
+    public Vector3 _direction;
+    public float _speed;
+
+    private Vector3 IsoVectorConvert(Vector3 vector)
+    {
+        Quaternion rotation = Quaternion.Euler(0, 45.0f, 0);
+        Matrix4x4 isoMatrix = Matrix4x4.Rotate(rotation);
+        Vector3 result = isoMatrix.MultiplyPoint3x4(vector);
+        return result;
+    }
+
+    public void OnMove(InputAction.CallbackContext context)
+    {
+        Vector2 readVector = context.ReadValue<Vector2>();
+        Vector3 toConvert = new Vector3(readVector.x, 0, readVector.y);
+        _direction = IsoVectorConvert(toConvert);
+    }
+
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        controller.Move(_direction * _speed * Time.deltaTime);
+        transform.rotation = Quaternion.LookRotation(_direction);
+    }
+}
