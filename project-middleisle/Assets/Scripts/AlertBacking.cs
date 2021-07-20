d51ee1b51668bdcb2bdecb0fdd3f777b7e105eab
PhongTrinh7 <phongtrinhvi@gmail.com>
2021-07-19 20:21:33 -0700

    Alert Tracking
    - make alerts track with the player

--- project-middleisle/Assets/Scripts/AlertBacking.cs
+++ project-middleisle/Assets/Scripts/AlertBacking.cs
@@ -5,12 +5,21 @@ using UnityEngine.UI;
 
 public class AlertBacking : MonoBehaviour
 {
+    public Transform player;
     public RectTransform background;
     public Text alertText;
     public float padding;
 
     public void Resize()
     {
+        Vector2 v = (Vector2)Camera.main.WorldToScreenPoint(player.position) + new Vector2(0, 150);
+        background.position = v;
         background.sizeDelta = new Vector2(alertText.preferredWidth + padding, alertText.fontSize + 10);
+    }
+
+    private void Update()
+    {
+        Vector2 v = (Vector2) Camera.main.WorldToScreenPoint(player.position) + new Vector2(0, 150);
+        background.position = v;
     }
 }
