2e6b4d98ba7811a1f0eedaf540496885aef72833
PhongTrinh7 <phongtrinhvi@gmail.com>
2021-07-20 21:35:49 -0700

    Alert Tracking
    - improve tracking to scale with screen size

--- project-middleisle/Assets/Scripts/AlertBacking.cs
+++ project-middleisle/Assets/Scripts/AlertBacking.cs
@@ -5,19 +5,23 @@ using UnityEngine.UI;
 
 public class AlertBacking : MonoBehaviour
 {
+    public Canvas canvas;
     public Transform player;
     public RectTransform background;
     public Text alertText;
     public float padding;
+    public float alertDistance = 200f;
 
     public void Resize()
     {
+        Vector2 v = (Vector2)Camera.main.WorldToScreenPoint(player.position) + new Vector2(0, alertDistance) * canvas.scaleFactor;
+        background.position = v;
         background.sizeDelta = new Vector2(alertText.preferredWidth + padding, alertText.fontSize + 10);
     }
 
     private void Update()
     {
-        Vector2 v = (Vector2)Camera.main.WorldToScreenPoint(player.position) + new Vector2(0, 150);
+        Vector2 v = (Vector2)Camera.main.WorldToScreenPoint(player.position) + new Vector2(0, alertDistance) * canvas.scaleFactor;
         background.position = v;
     }
 }
