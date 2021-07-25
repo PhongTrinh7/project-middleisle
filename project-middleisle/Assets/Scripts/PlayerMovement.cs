b5d403bffd313f8004ed511245999c19616ae30b
JGSides <67124891+JGSides@users.noreply.github.com>
2021-07-24 20:21:33 -0700

    Tested Animations, Adapted Animation Sandbox Scene

--- project-middleisle/Assets/Scripts/PlayerMovement.cs
+++ project-middleisle/Assets/Scripts/PlayerMovement.cs
@@ -1,64 +1,87 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
-public class PlayerMovement : MonoBehaviour
+public class PlayerMovement : MonoBehaviour, IsoMove.IPlayerActions
 {
-    //public GameObject player;
-    private Rigidbody playerRigid;
-
-
-    public float movementSpeed;
-    public float speedMultiplier;
-
-    public float jumpForce;
-
-    private bool jump;
-
-
+    public Animator animator;
+    public CharacterController controller;
+    public Vector3 _direction;
+    public float _speed;
+    public float _sprintSpeed;
 
     // Start is called before the first frame update
     void Start()
     {
-        playerRigid = GetComponent<Rigidbody>();
-        jump = false;
+        animator = GetComponent<Animator>();
+        controller = GetComponent<CharacterController>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        Movement();
-        if (Input.GetButtonDown("Jump"))
+
+        controller.Move(_direction * _speed * Time.deltaTime);
+        if (_direction.magnitude > 0.01)
         {
-            jump = true;
+            animator.SetBool("IsWalking", true);
+            transform.rotation = Quaternion.LookRotation(_direction);
         }
         else
         {
-            jump = false;
+            animator.SetBool("IsWalking", false);
         }
-
     }
 
-
-    void Movement()
+    public void OnMove(InputAction.CallbackContext context)
     {
+        Vector2 readVector = context.ReadValue<Vector2>();
+        Vector3 toConvert = new Vector3(readVector.x, 0, readVector.y);
+        _direction = IsoVectorConvert(toConvert);
+        // Debug.LogError("walking");
+    }
 
-        if (jump)
+    private Vector3 IsoVectorConvert(Vector3 vector)
+    {
+        Quaternion rotation = Quaternion.Euler(0, 45.0f, 0);
+        Matrix4x4 isoMatrix = Matrix4x4.Rotate(rotation);
+        Vector3 result = isoMatrix.MultiplyPoint3x4(vector);
+        return result;
+    }
+
+    public void OnSprint(InputAction.CallbackContext context)
+    {
+        _speed = _speed + _sprintSpeed;
+        animator.SetBool("IsRunning", true);
+        // Debug.LogError("running");
+    }
+
+    public void OnSprintFinish(InputAction.CallbackContext context)
+    {
+        _speed = _speed - _sprintSpeed;
+        if (context.performed)
         {
-            playerRigid.velocity = new Vector3(movementSpeed * speedMultiplier * Input.GetAxisRaw("Horizontal"), jumpForce, movementSpeed * speedMultiplier * Input.GetAxisRaw("Vertical"));
+            animator.SetBool("IsRunning", false);
         }
-        else
-        {
-            playerRigid.velocity = new Vector3(movementSpeed * speedMultiplier * Input.GetAxisRaw("Horizontal"), playerRigid.velocity.y, movementSpeed * speedMultiplier * Input.GetAxisRaw("Vertical"));
-        }
+    }
 
+    public void OnInventory(InputAction.CallbackContext context)
+    {
+        throw new System.NotImplementedException();
+    }
 
+    public void OnSkip(InputAction.CallbackContext context)
+    {
+        throw new System.NotImplementedException();
+    }
 
+    public void OnSkipFinish(InputAction.CallbackContext context)
+    {
+        throw new System.NotImplementedException();
+    }
 
-
-
-
-
-
+    public void OnSelect(InputAction.CallbackContext context)
+    {
+        throw new System.NotImplementedException();
     }
 }
