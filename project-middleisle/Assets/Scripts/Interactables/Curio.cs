9240e851f096f91eb45d3786e0b7135e69c2f2a6
PhongTrinh7 <phongtrinhvi@gmail.com>
2021-10-10 23:37:56 -0700

    Modular Dialogue

--- project-middleisle/Assets/Scripts/Interactables/Curio.cs
+++ project-middleisle/Assets/Scripts/Interactables/Curio.cs
@@ -4,26 +4,20 @@ using UnityEngine;
 
 public class Curio : Interactable
 {
-    public Sprite characterPortrait;
-    public string[] strings; //Use the inspector to place the dialogue in here with each textbox being a single element of the array.
-    public Queue<string> dialogue;
+    public Dialogue[] dialogues; // Use the inspector to place the dialogue in here with each textbox being a single element of the array.
+    public Queue<Dialogue> dialogue;
 
     private void Start()
     {
-        dialogue = new Queue<string>();
+        dialogue = new Queue<Dialogue>();
     }
 
     public override void Interact()
     {
-        if (characterPortrait != null)
-        {
-            DialogueManager.Instance.SetRightSidePortrait(characterPortrait);
-        }
-
         // This is in case curios can be interacted with more than once.
-        foreach (string s in strings)
+        foreach (Dialogue d in dialogues)
         {
-            dialogue.Enqueue(s);
+            dialogue.Enqueue(d);
         }
 
         StartCoroutine(DialogueManager.Instance.StartDialogue(dialogue));
