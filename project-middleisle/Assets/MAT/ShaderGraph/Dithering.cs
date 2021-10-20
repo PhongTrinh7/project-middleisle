97d792795d179ba7b6e8efc54f9768b868ba8e65
PhongTrinh7 <phongtrinhvi@gmail.com>
2021-10-20 14:17:11 -0700

    Add dithering to objects occluding character

---
+++ project-middleisle/Assets/MAT/ShaderGraph/Dithering.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dithering : MonoBehaviour
+{
+    public float ditherOpacity = 0.75f;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        other.GetComponent<Renderer>().material.SetFloat("_Opacity", ditherOpacity);
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        other.GetComponent<Renderer>().material.SetFloat("_Opacity", 1);
+    }
+}
