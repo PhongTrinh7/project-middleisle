f04cf105f5549d758987abd384a0b2d2d12d1974
JGSides <67124891+JGSides@users.noreply.github.com>
2021-11-21 22:46:44 -0800

    Lighting Comps and colliders

---
+++ project-middleisle/Assets/Scripts/FlickerControl.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlickerControl : MonoBehaviour
+{
+    public bool isFlickering = false;
+    public float timeDelay;
+    public float flickerFrequency = 0.03f;
+
+    void Update()
+    {
+        if(isFlickering == false)
+        {
+            StartCoroutine(FlickeringLight());
+        }
+    }
+
+    IEnumerator FlickeringLight()
+    {
+        isFlickering = true;
+        this.gameObject.GetComponent<Light>().enabled = false;
+        timeDelay = Random.Range(0.01f, flickerFrequency);
+        yield return new WaitForSeconds(timeDelay);
+        this.gameObject.GetComponent<Light>().enabled = true;
+        timeDelay = Random.Range(0.01f, flickerFrequency);
+        yield return new WaitForSeconds(timeDelay);
+        isFlickering = false;
+    }
+}
